b1697b0c52fd25e925ebed8a51ddc86156d713b5
jiandandian <241880352@smail.nju.edu.cn>
2024-12-15 10:09:16 +0800

    use to print "hello world"

---
+++ CPL.c
@@ -0,0 +1,10 @@
+//
+// Created by jiandandian on 24-12-15.
+//
+#include <stdio.h>
+int main(){
+  int a;
+  scanf("%d",&a);
+  printf("Hello World\n");
+  printf("%d",a);
+}
---
+++ CPL.cu
@@ -0,0 +1,9 @@
+//
+// Created by jiandandian on 24-12-15.
+//
+
+#include "CPL.cuh"
+#include <stdio.h>
+int main(){
+  printf("Hello World\n");
+}
